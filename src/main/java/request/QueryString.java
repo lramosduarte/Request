f688636682e3609091768507986d8ac28dce7593
Leonardo Ramos Duarte <lramosduarte@gmail.com>
2016-11-09 16:24:58 -0200

    class para manipular dados transformando em uma querystring correta

---
+++ src/main/java/request/QueryString.java
@@ -0,0 +1,14 @@
+package main.java.request;
+
+import java.util.LinkedHashMap;
+
+/**
+ * Created by sisqualis on 09/11/16.
+ */
+public class QueryString {
+
+    public String getQueryParams(LinkedHashMap<String, String> parametros) {
+        return null
+    }
+
+}
